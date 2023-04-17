f8472021cf8c3c26bdd352131821f0ca997e95c8
Egor <egor.dmitriev99@gmail.com>
2023-04-17 02:19:19 +0200

    grid size submition in Progress

--- scripts.js
+++ scripts.js
@@ -1,9 +1,48 @@
 const grid = document.querySelector('.grid')
+const submit = document.getElementById('submit')
 
-for ( let i=0; i<16;i++){
-    for (let j = 0; j<16; j++){
+submit.addEventListener('click', () => squareadder())
+for ( let i=0; i<11;i++){
+    for (let j = 0; j<11; j++){
         let elem = document.createElement('div')
         elem.classList.add('square')
         grid.appendChild(elem)
     }
+}
+
+let square = document.querySelectorAll('.square')
+
+for (const i of square){
+    i.addEventListener('mouseover', () => {
+        i.classList.add('squareVisited')
+    });
+}
+
+function squareadder(){
+    const x = document.forms['myform']['fname'].value;
+
+    for (const i of square){
+        i.remove()
+    }
+
+    for ( let i=0; i<x;i++){
+        for (let j = 0; j<x; j++){
+            let elem = document.createElement('div')
+            elem.classList.add('square')
+            grid.appendChild(elem)
+        }
+    }
+    
+    square = document.querySelectorAll('.square');
+
+
+
+    for (const i of square){
+        i.style.Width = `${352 / x}px`;
+        i.style.Height = `${352 / x}px`;
+        i.addEventListener('mouseover', () => {
+            i.classList.add('squareVisited')
+        });
+    }
+
 }
